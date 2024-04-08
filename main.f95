781b315adad47b4c1fb34dee7ccf80e6152d4c69
Paul Saletzki <39807566+FractalPaul@users.noreply.github.com>
2024-04-08 08:45:19 -0700

    Main Pythagorean Triplets calculation
    Calculates the Pythagorean Triplets and outputs the values to a csv file.

---
+++ main.f95
@@ -0,0 +1,29 @@
+program PythagoreanTriplets
+    ! Calculate a given number of Pythagorean Triplets.
+    ! Find whole (integer) numbers that satisfy the a^2 + b^2 = c^2 Pythagorean Triplets.
+    ! Paul Saletzki
+    ! April 7, 2024
+    implicit none
+
+    ! Declare variables
+    integer :: m, n, a, b, c, maxNum
+    integer :: io
+
+    ! Prepare file to output the Triplets.
+    open(newunit=io, file='Pythagorean Triplets.csv', status="old", action="write")
+    write(io,*) 'a,', 'b,', 'c'
+
+    ! Calculate the Pythagorean Triplets given a number to go up to for calculating the triplets.
+    maxNum = 20
+    do m = 1,maxNum
+        do n = 1, m-1
+            a = m**2 - n**2
+            b = 2*m*n
+            c = m**2 + n**2
+            print *, '(', a,',',b,',',c,')'
+            write(io,*) a, ',', b,',', c
+        end do
+    end do
+    close(io)
+
+end program PythagoreanTriplets
